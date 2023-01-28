152b6b8d6ee1f0c5c0fb58dc94f06c92ad75002c
Neil Park <neil.park.st@gmail.com>
2023-01-28 22:00:11 +0900

    blog frontend initial setting

---
+++ blog-frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+
+const App = () => {
+	return (
+		<Routes>
+			<Route element={<PostListPage />} path="/" />
+			<Route element={<PostListPage />} path="/:username" />
+			<Route element={<LoginPage />} path="/login/*" />
+			<Route element={<RegisterPage />} path="/register/*" />
+			<Route element={<WritePage />} path="/write/*" />
+			<Route element={<PostPage />} path="/:username/:postId/*" />
+		</Routes>
+	);
+};
+
+export default App;
