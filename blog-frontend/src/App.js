3d0a92df522f397cf1fa9edabff44ec7c78d4bae
Neil Park <neil.park.st@gmail.com>
2023-02-08 22:28:18 +0900

    add a function of handling 'head' of html

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import PostListPage from './pages/PostListPage';
@@ -8,14 +9,19 @@ import WritePage from './pages/WritePage';
 
 const App = () => {
 	return (
-		<Routes>
-			<Route element={<PostListPage />} path="/" />
-			<Route element={<PostListPage />} path="/:username" />
-			<Route element={<LoginPage />} path="/login/*" />
-			<Route element={<RegisterPage />} path="/register/*" />
-			<Route element={<WritePage />} path="/write/*" />
-			<Route element={<PostPage />} path="/:username/:postId/*" />
-		</Routes>
+		<>
+			<Helmet>
+				<title>Blog Project</title>
+			</Helmet>
+			<Routes>
+				<Route element={<PostListPage />} path="/" />
+				<Route element={<PostListPage />} path="/:username" />
+				<Route element={<LoginPage />} path="/login/*" />
+				<Route element={<RegisterPage />} path="/register/*" />
+				<Route element={<WritePage />} path="/write/*" />
+				<Route element={<PostPage />} path="/:username/:postId/*" />
+			</Routes>
+		</>
 	);
 };
 
